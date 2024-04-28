87742fe4126f4fa970eac8bb8d2cbcce4f0b2bad
sepide <160308636+sepideSemiari@users.noreply.github.com>
2024-04-28 18:29:02 +0330

    update

--- src/main/java/org/example/AtmSystem/util/JDBC.java
+++ src/main/java/org/example/AtmSystem/util/JDBC.java
@@ -1,2 +1,28 @@
-package org.example.AtmSystem.util;public class JDBC {
+package org.example.AtmSystem.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBC implements AutoCloseable {
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "1";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Connection connection;
+
+    @Override
+    public void close() throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
 }
--- src/main/java/org/example/AtmSystem/util/Regex.java
+++ src/main/java/org/example/AtmSystem/util/Regex.java
@@ -1,2 +1,9 @@
-package org.example.AtmSystem.util;public class Regex {
+package org.example.AtmSystem.util;
+
+public class Regex {
+    public final static String National_Code="\\d{10}";
+    public final static String name="^\\b([A-Za-z.]+\\s?)+$";
+    public final static String lastName="^\\b([A-Za-z.]+\\s?)+$";
+    public final static String address="^\\b([A-Za-z.]+\\s?)+$";
+
 }
